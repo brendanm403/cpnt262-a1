e68e3e5b5f3ac48d29fdfcaf850de1ed740468f5
Brendan Malycky <bmalycky@hotmail.com>
2023-11-20 21:31:32 -0700

    fetched data needed for gallery page

--- second.js
+++ second.js
@@ -1,1 +1,20 @@
-console.log(localStorage.getItem("coins"));
+let pokemonArr = [];
+
+const fetchPokemon = () => {
+  let promises = [];
+  for (let i = 1; i <= 151; i++) {
+    const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+      promises.push(fetch(url).then((res) => res.json()));
+    }  
+
+  Promise.all(promises).then((results) => {
+     pokemonArr = results.map((data) => ({
+      name: data.name,
+      src: data.sprites.versions["generation-v"]["black-white"].front_default,
+      id: data.id
+    }));
+    console.log(pokemonArr);
+  });  
+}
+
+fetchPokemon();
